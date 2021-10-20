ccb838d9169b9783562bff433ed8675d84d54c7a
fanhuidan <fanhuidan@cloudwalk.cn>
2021-10-20 17:36:00 +0800

    redux demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { createStore } from "redux";
+
 import "./App.css";
 
 function App() {
+  const defaultState = {
+    welcome: "Hello",
+    otherState: "other...",
+  };
+  const greeting = (state = defaultState, action) => {
+    switch (action.type) {
+      case "GREET_ME":
+        return { ...state, welcome: "Hello Holiday" };
+      case "GREET_WORLD":
+        return { ...state, welcome: "Hello World" };
+      default:
+        return state;
+    }
+  };
+  const store = createStore(greeting);
+  console.log(store, store.getState());
+
+  store.dispatch({ type: "GREET_WORLD" });
+  console.log(store.getState());
+
   return <div className="App">App</div>;
 }
 
